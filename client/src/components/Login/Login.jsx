211473da24e4a45304eff125f32b53bc21c110de
k-hongkham <k.hongkham@gmail.com>
2022-06-07 13:45:51 -0400

    started db.

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,8 +5,23 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [Error, setError] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState("");
 
-  return <div id="login_portal"></div>;
+  return (
+    <div id="login_portal">
+      <LoginForm
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        ErrorMessage={ErrorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
+  );
 };
 
 export default Login;
--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,10 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useAuth from "../hooks/useAuth";
+import { userLogin } from "../../axios";
 
-const LoginForm = () => {
+const LoginForm = (
+  username,
+  setUsername,
+  password,
+  setPassword,
+  isLoggedIn,
+  setIsLoggedIn
+) => {
   const { setToken } = useAuth();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await userLogin(username, password);
+
+      localStorage.setItem("token", response.token);
+      setToken(response.token);
+      setIsLoggedIn(false);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <h2>Login</h2>
+      </header>
+      <div>
+        <form>
+          <div>
+            <input
+              type="text"
+              name="login-Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="login-Username">Username</label>
+          </div>
+          <div>
+            <input
+              type="password"
+              name="login-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="login-password">Password</label>
+          </div>
+          <button onClick={(e) => handleLogin(e)}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
