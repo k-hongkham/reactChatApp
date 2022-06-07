ef5057b67455f5ddf8d847d2283871653b50ef1c
k-hongkham <k.hongkham@gmail.com>
2022-06-07 13:00:11 -0400

    added authProvider for user

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
 
 const LoginForm = () => {
   const { setToken } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 };
 
 export default LoginForm;
