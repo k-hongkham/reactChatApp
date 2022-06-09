5e17110c396ad716b5ff381eb24901b49a7bfcc7
k-hongkham <k.hongkham@gmail.com>
2022-06-09 11:12:16 -0400

    added db fns for getting all channels and updating channels.

--- db/models/channel.js
+++ db/models/channel.js
@@ -2,6 +2,8 @@ const client = require("../client");
 
 module.exports = {
   createChannel,
+  getAllChannels,
+  updateChannel,
 };
 
 async function createChannel({ channelId, name }) {
@@ -15,6 +17,46 @@ async function createChannel({ channelId, name }) {
         RETURNING *;
         `,
       [channelId, name]
+    );
+    return channel;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllChannels() {
+  try {
+    const { rows } = await client.query(`
+        SELECT
+        channels.id AS id,
+        channels.name AS name
+        FROM channels
+        ORDER BY channels.id desc
+        `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateChannel(fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, idx) => `'${key}'=$${idx + 1}`)
+    .join(", ");
+  if (setString.length === 0) {
+    return;
+  }
+  try {
+    const {
+      rows: [channel],
+    } = await client.query(
+      `
+        UPDATE channels
+        SET ${setString}
+        WHERE id=${fields.id}
+        RETURNING *;
+        `,
+      Object.values(fields)
     );
     return channel;
   } catch (error) {
